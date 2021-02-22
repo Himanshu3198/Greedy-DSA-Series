c74af83a23f6693c783286081e97150f72c097da
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-02-22 23:38:38 +0530

    Update GreedyAcitivitySelection.cpp

--- GreedyAcitivitySelection.cpp
+++ GreedyAcitivitySelection.cpp
@@ -1,35 +1,61 @@
+
+// activity selection problem
+// time complexity O(nlogn)+O(n)
 #include<bits/stdc++.h>
-using namespace std;
-struct Activity{
-	int start,finish;
-};
-//      bool activityCompare(Activity s1, Activity s2) 
-//{ 
-//    return (s1.finish <s2.finish); 
-//} 
-      void printmaxActivities(Activity arr[],int n)
-      {
-//      	sort(arr,arr+n,activityCompare);
-sort(arr,arr+n);
-	  
-	  int i=0;
-	  cout<<"("<<arr[i].start<<","<<arr[i].finish<<") ,";
-	  for(int j=1;j<n;j++)
-	  {
-	  	if(arr[j].start>=arr[i].finish)
-	  	{
-	  		cout<<"("<<arr[j].start<<","<<arr[j].finish<<"),";
-	  		i=j;
-		  }
-	  }
+using namespace  std;
+ // } Driver Code Ends
+
+
+bool myCmp(pair<int, int> a, pair<int, int> b)
+{
+    return a.second < b.second;
+}
+
+int activitySelection(int start[], int end[], int n) {
+    
+    vector<pair<int, int>> jobTime;
+    
+    for(int i = 0; i < n; i++) {
+        jobTime.push_back({start[i], end[i]});
+    }
+    
+    sort(jobTime.begin(), jobTime.end(), myCmp);
+
+    int count = 1;
+    pair<int, int> last = jobTime[0];
+    
+    for(int i = 1; i < n; i++)
+    {
+        if(jobTime[i].first >= last.second)
+        {
+            count++;
+            last = jobTime[i];
+        }
+        
+    }
+    
+    return count;
 }
 
 
+// { Driver Code Starts.
 int main()
-  {
-  	Activity arr[]={{5,9}, {1, 2}, {3, 4}, {0, 6},{5, 7}, {8, 9}};
-  	   int n=sizeof(arr)/sizeof(arr[0]);
-  	   printmaxActivities(arr,n);
-  	   return 0;
-  
-  }
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>start[i];
+        for(int i=0;i<n;i++)
+            cin>>end[i];
+        
+        cout << activitySelection(start, end, n) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
