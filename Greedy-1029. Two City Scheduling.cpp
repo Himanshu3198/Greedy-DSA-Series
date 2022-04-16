22fe02d143120e1061f95e33a756b94f33742233
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-04-16 23:48:51 +0530

    Update Greedy-1029. Two City Scheduling.cpp

--- Greedy-1029. Two City Scheduling.cpp
+++ Greedy-1029. Two City Scheduling.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
+//      time complexity(nlog(n))
+//     in this problem sorting by first value or second will not work because we don't 
+//     known which value first or second is mininum therefore we sort by it difference
+//     is descending order when value is greater than one choose first value else second
+    static bool cmp(const vector<int>&v1,const vector<int>&v2){
+        return (v1[1]-v1[0])>(v2[1]-v2[0]);
+    }
     int twoCitySchedCost(vector<vector<int>>& costs) {
         
-        
         int n=costs.size();
-        sort(costs.begin(),costs.end());
-        
-        
-        int res1=0,res2=0;
+        sort(costs.begin(),costs.end(),cmp);
+        int result=0;
         for(int i=0;i<n;i++){
-            
-            res1+=costs[i][0];
-            
+            if(i<n/2)
+                result+=costs[i][0];
+            else
+                result+=costs[i][1];
         }
-        for(int i=0;i<n;i++){
-            res2+=costs[i][1];
-        }
-        return min(res1,res2);
+        return result;
+      
     }
 };
