38624f8b8da0adace6060fbdd595cee8d603dddc
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-07-04 23:15:04 +0530

    Update Greedy-Candy.cpp

--- Greedy-Candy.cpp
+++ Greedy-Candy.cpp
@@ -1,33 +1,28 @@
-135. Candy
-
-
 class Solution {
 public:
-    int candy(vector<int>& nums) {
-        int n=nums.size();
+    
+    // t.c O(n)
+    // s.c O(2n)
+    int candy(vector<int>& ratings) {
         
+        int n=ratings.size();
         vector<int>left(n,1);
         vector<int>right(n,1);
-        int ans=0;
-        
+        int res=0;
         for(int i=1;i<n;i++){
             
-            if(nums[i]>nums[i-1]){
+            if(ratings[i]>ratings[i-1]){
                 left[i]=left[i-1]+1;
             }
         }
-        
         for(int i=n-2;i>=0;i--){
-            
-            if(nums[i]>nums[i+1]){
+            if(ratings[i]>ratings[i+1]){
                 right[i]=right[i+1]+1;
             }
         }
-        
         for(int i=0;i<n;i++){
-            ans+=max(left[i],right[i]);
+            res+=max(left[i],right[i]);
         }
-        
-        return ans;
+        return res;
     }
 };
