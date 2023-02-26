f4a89acf19cbcc0e1318c6f67eabab7aa9060b5a
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2023-02-26 13:48:55 +0530

    Create Greedy-2576. Find the Maximum Number of Marked Indices.cpp

---
+++ Greedy-2576. Find the Maximum Number of Marked Indices.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxNumOfMarkedIndices(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size(),ans=0,i=0,j=n/2;
+        while(i<n/2 && j<n){
+            if(2*nums[i]<=nums[j]){
+                i++;
+                j++;
+                ans+=2;
+            }else j++;
+        }
+        return ans;
+    }
+};
